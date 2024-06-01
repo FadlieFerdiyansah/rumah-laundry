0aced9bf0d1be7140aec00a70f23b8f1d26510d3
fadlieferdiyansah <fadlieferdiyansah26@gmail.com>
2024-06-01 21:35:39 +0700

    update

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -121,61 +121,30 @@
 										<a href="{{ $transaksis->payment_url }}" target="_blank" class="btn btn-sm btn-info">Bayar</a>
 									@elseif(in_array($transaksis->payment_code, ['bank_bca','bank_mandiri', 'bank_bri']))
 										<a href="" data-toggle="modal"  data-target="#infobank_{{ $transaksis->invoice }}" class="btn btn-sm btn-info">Bayar</a>
-										<div class="modal fade" id="infobank_{{ $transaksis->invoice }}" tabindex="-1" role="dialog" aria-labelledby="infobank" aria-hidden="true">
+										<div class="modal fade text-left" id="infobank_{{ $transaksis->invoice }}" tabindex="-1" role="dialog" aria-labelledby="bayar" aria-hidden="true">
 											<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
 												<div class="modal-content">
 													<div class="modal-header">
-														<h4 class="modal-title">Pembayaran</h4>
+														<h4 class="modal-title">Pembayaran </h4>
 														<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 															<span aria-hidden="true">&times;</span>
 														</button>
 													</div>
-													<div class="modal-body">
-														<h5 class="text-center mb-2"><strong>Informasi Bank Transfer</strong></h5>
-														<p>Untuk melakukan pembayaran melalui transfer bank, silakan gunakan informasi berikut:</p>
-														{{-- <table class="table table-borderless">
-															<tbody>
-																<tr>
-																	<td><strong>Nama Bank:</strong></td>
-																	<td>{{ $transaksis->bank($transaksis->payment_method)->nama_bank }}</td>
-																</tr>
-																<tr>
-																	<td><strong>Nama Rekening:</strong></td>
-																	<td>{{ $transaksis->bank($transaksis->payment_method)->nama_pemilik }}</td>
-																</tr>
-																<tr>
-																	<td><strong>Nomor Rekening:</strong></td>
-																	<td>{{ $transaksis->bank($transaksis->payment_method)->no_rekening }}</td>
-																</tr>
-																<tr>
-																	<td><strong>Berita Acara:</strong></td>
-																	<td>[{{ $transaksis->customers->name }}] - [{{ $transaksis->invoice }}]</td>
-																</tr>
-															</tbody>
-														</table> --}}
-														<p>Pastikan berita acara sesuai agar pembayaran cepat diproses. Untuk pertanyaan, hubungi tim dukungan kami.</p>
-														<div class="d-flex flex-row align-items-center">
-															<a href="https://api.whatsapp.com/send?phone=6281234567890" target="_blank" class="btn btn-outline-success mr-2">
-																<i class="fa fa-whatsapp"></i>
-															</a>
-															<a href="tel:{{ $setting->no_telp }}" class="btn btn-outline-primary mr-2">
-																<i class="fa fa-phone"></i>
-															</a>
-															<a href="{{ $setting->facebook }}" target="_blank" class="btn btn-outline-info mr-2">
-																<i class="fa fa-facebook"></i>
-															</a>
-															<a href="mailto:{{ $setting->email }}" class="btn btn-outline-secondary">
-																<i class="fa fa-envelope"></i>
-															</a>
-														</div>
-														
-													</div>
-													<div class="modal-footer">
-														<button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
+													<form action="{{route('bayar')}}" method="POST">
+													  @csrf
+													  <div class="modal-body">
+															ini test
 													  </div>
+													  <div class="modal-footer">
+														<button type="submit" class="btn btn-primary">Submit</button>
+														<button type="button" class="btn btn-info" data-dismiss="modal">Cancel</button>
+													  </div>
+													</form>
 												</div>
 											</div>
-										</div>
+										  </div>
+										
+										
 									@elseif(in_array($transaksis->payment_code, ['BC','M2']))
 										<a href="" data-toggle="modal" data-invoice="{{ $transaksis->invoice }}" data-harga="{{ Rupiah::getRupiah($transaksis->harga_akhir) }}" data-target="#bayar" class="btn btn-sm btn-info">Bayar</a>
 									@endif
