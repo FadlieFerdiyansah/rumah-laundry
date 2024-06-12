0bf29cdefbac2637a9cc60d512626ab0e254c5c9
fadlieferdiyansah <fadlieferdiyansah26@gmail.com>
2024-06-12 17:35:31 +0700

    update

--- resources/views/frontend/content.blade.php
+++ resources/views/frontend/content.blade.php
@@ -1,5 +1,56 @@
 {{-- Content --}}
 {{-- <contenone></contenone> --}}
 <br><br><br><br>
-<contentwo></contentwo>
+{{-- <contentwo></contentwo> --}}
+<template>
+  <div>
+    <h3 class="text-center" style="color: black;">Mengapa Kami ?</h3>
+    <hr>
+    <div class="row">
+      <div class="col-md-3 promo">
+        <div class="panel panel-forum">
+          <div class="panel-heading">
+              <h4 class="panel-title" style="font-weight:bold; color:black; font-size:15px">
+                  Pembayaran Mudah
+              </h4>
+          </div>
+          <img :src="'frontend/img/order.png'" style="width: 100%; max-height:230px; min-height:230px" alt="">
+        </div>
+      </div>
+
+      <div class="col-md-3 shadow-lg">
+        <div class="panel panel-forum tips">
+          <div class="panel-heading">
+              <h4 class="panel-title" style="font-weight:bold; color:black; font-size:15px">
+                  Profesional
+              </h4>
+          </div>
+          <img :src="'frontend/img/profesional.png'" style="width: 100%; max-height:230px; min-height:230px" alt="">
+        </div>
+      </div>
+
+      <div class="col-md-3 shadow-lg">
+        <div class="panel panel-forum tips">
+          <div class="panel-heading">
+              <h4 class="panel-title" style="font-weight:bold; color:black; font-size:15px">
+                  Terpercaya
+              </h4>
+          </div>
+          <img :src="'frontend/img/terpercaya.png'" style="width: 100%; max-height:230px; min-height:230px" alt="">
+        </div>
+      </div>
+
+      <div class="col-md-3 shadow-lg">
+        <div class="panel panel-forum tips">
+          <div class="panel-heading">
+              <h4 class="panel-title" style="font-weight:bold; color:black; font-size:15px">
+                  Tepat Waktu
+              </h4>
+          </div>
+          <img :src="'frontend/img/garansi.png'" style="width: 100%; max-height:230px; min-height:230px" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 {{-- End Content --}}
